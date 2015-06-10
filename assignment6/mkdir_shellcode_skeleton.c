b2856cd5fd785ac36416838a47e008506df21cf5
muttiopenbts <muttiopenbts@users.noreply.github.com>
2015-06-10 01:33:33 -0700

    Create mkdir_shellcode_skeleton.c

---
+++ assignment6/mkdir_shellcode_skeleton.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+/*
+	mkdir called 'hacked' shellcode
+*/
+unsigned char code[] = 
+"\xeb\x16\x5e\x31\xc0\x88\x46\x06\xb0\x27\x8d\x1e\x66\xb9\xed\x01\xcd\x80\xb0\x01\x31\xdb\xcd\x80\xe8\xe5\xff\xff\xff\x68\x61\x63\x6b\x65\x64\x23";
+
+main()
+{
+    printf("Shellcode Length: %d\n", sizeof(code)-1);
+    int (*ret)() = (int(*)())code;
+    ret();
+}
